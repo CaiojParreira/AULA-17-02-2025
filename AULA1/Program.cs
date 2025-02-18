7e7f6d34f1714ace919b164718d9450514c27b93
048525 <048525@UNI-LAB2-40>
2025-02-18 19:33:44 -0300

    Adicionar arquivos de projeto.

---
+++ AULA1/Program.cs
@@ -0,0 +1,14 @@
+﻿double resultado, x, y, cal_x, cal_y, elevado_x, elevado_y;
+
+x=int.Parse(Console.ReadLine());
+y=int.Parse(Console.ReadLine());
+
+cal_x = (x * 2) - (x * 1);
+cal_y = (y * 2) + (y * 1);
+
+elevado_x = Math.Pow(cal_x, 2);
+elevado_y = Math.Pow(cal_y, 2);
+
+resultado = elevado_x + elevado_y;
+
+Console.WriteLine(Math.Sqrt(resultado));
